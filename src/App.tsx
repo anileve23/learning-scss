5e684453d5f735812caaeaa37e649b9cde64ae03
Evelinarudz <95478655+Evelinarudz@users.noreply.github.com>
2025-02-14 13:21:49 +0200

    Fixing url refresh error

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   const currentTheme = themeContext?.theme || 'light';
 
   return (
-    <Router basename="/learning-scss">
+    <Router>
       <div className={currentTheme}>
         <Navbar />
         <main className="content">
